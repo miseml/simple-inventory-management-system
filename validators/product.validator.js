62a250f99cd0b107695e6e9fe3abb86520130011
miseml <semla.michal@g.pl>
2026-01-06 19:47:26 +0100

    Added express validator package and implemented product validator

---
+++ validators/product.validator.js
@@ -0,0 +1,19 @@
+import { body } from "express-validator";
+
+export const createProductValidator = [
+    body("name")
+        .notEmpty().withMessage("Name is required")
+        .isLength({ max: 50 }).withMessage("Name must be at most 50 characters"),
+
+    body("description")
+        .notEmpty().withMessage("Description is required")
+        .isLength({ max: 50 }).withMessage("Description must be at most 50 characters"),
+
+    body("price")
+        .notEmpty().withMessage("Price is required")
+        .isFloat({ gt: 0 }).withMessage("Price must be a positive number"),
+
+    body("stock")
+        .notEmpty().withMessage("Stock is required")
+        .isInt({ min: 0 }).withMessage("Stock must be a non-negative integer"),
+];
